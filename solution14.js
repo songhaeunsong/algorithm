d70b9856ab53d37f6d7b7494e64e574ff579dc82
songhaeunsong <gkdms3469@naver.com>
2022-03-30 16:36:17 +0900

    add: solution 13, 14

---
+++ solution14.js
@@ -0,0 +1,19 @@
+/*대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 
+다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 
+단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다. */
+
+function solution(s) {
+  let answer = true;
+  const arr = [...s];
+  let pnum = 0;
+  let ynum = 0;
+  for (let i = 0; i < arr.length; i++)
+    arr[i] === "p" || arr[i] === "P"
+      ? (pnum += 1)
+      : arr[i] === "y" || arr[i] === "Y"
+      ? (ynum += 1)
+      : null;
+  if (pnum === ynum) return answer;
+  else return !answer;
+}
