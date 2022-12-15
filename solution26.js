28e1f7ebd71602c4001910af9a5de00da0760154
songhaeunsong <gkdms3469@naver.com>
2022-12-15 14:42:12 +0900

    programmers sol 27

---
+++ solution26.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  let f0 = 0;
+  let f1 = 1;
+  let fn = 0;
+  for (let i = 2; i <= n; i++) {
+    fn = (f0 + f1) % 1234567;
+    f0 = f1;
+    f1 = fn;
+  }
+  return fn;
+}
