88f92bb245ffc859bc39c86d114f821433f39543
songhaeunsong <gkdms3469@naver.com>
2023-08-12 01:58:19 +0900

    sol: 의상

---
+++ Programmers/LV2/solution24.js
@@ -0,0 +1,15 @@
+function solution(clothes) {
+  let clothesNum = clothes.length;
+  let clothesObj = {};
+  let clothesArr = [];
+
+  clothes.forEach((item) => {
+    clothesObj[item[1]] ? clothesObj[item[1]]++ : (clothesObj[item[1]] = 1);
+  });
+
+  if (Object.keys(clothesObj).length === 1) return clothesNum;
+  clothesArr = Object.values(clothesObj);
+
+  clothesArr = clothesArr.reduce((acc, cur) => acc * (cur + 1), 1);
+  return clothesArr - 1;
+}
