d5dda50a2bc68e60eca50190261c147fba42b22f
songhaeunsong <gkdms3469@naver.com>
2023-10-13 22:39:35 +0900

    sol: 프로세스

---
+++ Programmers/LV2/solution27.js
@@ -0,0 +1,19 @@
+//프로세스
+
+function solution(priorities, location) {
+  let answer = 0;
+  const array = priorities.map((process, index) => {
+    return { process, index };
+  });
+
+  while (array.length) {
+    const queue = array.shift();
+    if (array.some((element) => element.process > queue.process)) {
+      array.push(queue);
+    } else {
+      answer++;
+      if (queue.index === location) break;
+    }
+  }
+  return answer;
+}
