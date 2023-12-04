24f7c89382a2ba016776b72e1d424a60e3b3766e
songhaeunsong <gkdms3469@naver.com>
2023-12-04 18:02:04 +0900

    sol: n^2 배열 자르기

---
+++ Programmers/LV2/solution32.js
@@ -0,0 +1,11 @@
+//n^2 배열 자르기
+
+function solution(n, left, right) {
+  const ans = [];
+
+  while (left <= right) {
+    ans.push(Math.max(Math.floor(left / n), left++ % n) + 1);
+  }
+
+  return ans;
+}
