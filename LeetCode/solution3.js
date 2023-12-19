e934524d5a28e65405c0628adba9ef2ce0d02776
songhaeunsong <gkdms3469@naver.com>
2023-12-19 23:54:10 +0900

    sol: Longest Palindromic Substring

--- LeetCode/solution3.js
+++ LeetCode/solution3.js
@@ -1,1 +1,61 @@
 // Longest Palindromic Substring
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  let firstWord = "";
+  let secondWord = "";
+
+  let checkPalindromic = (idx) => {
+    let count = 0;
+    let front = idx + 1;
+    let back = idx - 1;
+
+    while (back >= 0 && front <= s.length) {
+      if (s[front] === s[back]) {
+        count++;
+        front++;
+        back--;
+      } else return count;
+    }
+
+    return count;
+  };
+
+  let checkEvenPalindromic = (backIdx, frontIdx) => {
+    let count = 0;
+    let front = frontIdx + 1;
+    let back = backIdx - 1;
+
+    while (back >= 0 && front <= s.length) {
+      if (s[front] === s[back]) {
+        count++;
+        front++;
+        back--;
+      } else return count;
+    }
+
+    return count;
+  };
+
+  for (let idx = 0; idx < s.length; idx++) {
+    if (s[idx] === s[idx + 1]) {
+      let evenTries = checkEvenPalindromic(idx, idx + 1);
+      secondWord =
+        evenTries >= secondWord.length / 2
+          ? s.slice(idx - evenTries, idx + evenTries + 2)
+          : secondWord;
+    }
+
+    let oddTries = checkPalindromic(idx);
+    firstWord =
+      oddTries > Math.floor(firstWord.length / 2)
+        ? s.slice(idx - oddTries, idx + oddTries + 1)
+        : firstWord;
+  }
+  if (firstWord === "" && secondWord === "") return s[0];
+
+  return firstWord.length > secondWord.length ? firstWord : secondWord;
+};
