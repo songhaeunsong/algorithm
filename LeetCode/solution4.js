b7b289e89d6396cbb7db7ab45992cc0530b7add9
songhaeunsong <gkdms3469@naver.com>
2023-12-20 22:08:14 +0900

    sol: Zigzag Conversion

---
+++ LeetCode/solution4.js
@@ -0,0 +1,27 @@
+// Zigzag Conversion
+
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+  if (numRows === 1 || s.length <= numRows) {
+    return s;
+  }
+
+  const rows = new Array(numRows).fill("");
+  console.log(rows);
+  let currentRow = 0;
+  let goingDown = false;
+
+  for (const char of s) {
+    rows[currentRow] += char;
+    if (currentRow === 0 || currentRow === numRows - 1) {
+      goingDown = !goingDown;
+    }
+    currentRow += goingDown ? 1 : -1;
+  }
+
+  return rows.join("");
+};
