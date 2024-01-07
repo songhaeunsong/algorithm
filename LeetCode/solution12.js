10cb8cc99d8a0720dee179c4092f6dd407449edd
songhaeunsong <gkdms3469@naver.com>
2024-01-07 23:53:13 +0900

    3Sum 풀이 계획 작성

---
+++ LeetCode/solution12.js
@@ -0,0 +1,13 @@
+// 3Sum
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  // 오름차순 정렬
+  // for문
+  // 현재 인덱스 +1(first) 과 nums.length-1(last) sum 0인지 확인
+  // sum > 0 last--
+  // sum < 0 first++
+};
