62881aaf27665e26649a762dd1bf605f80317470
songhaeunsong <gkdms3469@naver.com>
2024-01-17 01:33:19 +0900

    sol: 1463 1로 만들기

--- BaekJoon/1463/solution.js
+++ BaekJoon/1463/solution.js
@@ -5,22 +5,15 @@ fileContent = fs.readFileSync("./input.txt").toString();
 let input = Number(fileContent);
 
 function solution(number) {
-  const dp = new Array(number + 1);
-  dp[1] = 0;
-  dp[2] = 1;
-  dp[3] = 1;
+  const dp = new Array(number + 1).fill(0);
 
-  if (dp[number]) return dp[number];
+  for (let i = 2; i <= number; i++) {
+    dp[i] = dp[i - 1] + 1;
 
-  for (let i = 4; i <= number; i++) {
-    let min = number;
-
-    if (i % 3 === 0) min = Math.min(min, dp[i / 3]);
-    if (i % 2 === 0) min = Math.min(min, dp[i / 2]);
-    min = Math.min(min, dp[i - 1]);
-
-    dp[i] = min + 1;
+    if (i % 2 === 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+    if (i % 3 === 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
   }
+
   return dp[number];
 }
 
