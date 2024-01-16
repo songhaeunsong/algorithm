9e2fb7e6894d169636cf76cdacfb34afc50ba47d
songhaeunsong <gkdms3469@naver.com>
2024-01-17 00:38:59 +0900

    sol: 2839 설탕 배달

--- BaekJoon/2839/solution.js
+++ BaekJoon/2839/solution.js
@@ -1,14 +1,18 @@
+// 설탕 배달 dp
+
 const fs = require("fs");
 fileContent = fs.readFileSync("./input.txt").toString();
 let input = Number(fileContent);
 // "/dev/stdin"
 function solution(target) {
-  const dp = [[3, 5]];
-  let amount = 0;
-
-  let fc = () => {
-    fc();
-  };
+  let amountOfThree = 0;
+  while (target >= 3) {
+    if (target % 5 === 0) return target / 5 + amountOfThree;
+    if (target === 3) return amountOfThree + 1;
+    amountOfThree++;
+    target -= 3;
+  }
+  return -1;
 }
 
 console.log(solution(input));
