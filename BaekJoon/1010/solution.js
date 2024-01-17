1c36559239344e4c2e245e42c846be43e830cdbf
songhaeunsong <gkdms3469@naver.com>
2024-01-18 02:25:52 +0900

    sol: 1010 다리 놓기

---
+++ BaekJoon/1010/solution.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+const [caseLength, ...fileContent] = fs
+  .readFileSync("./input.txt", "utf-8")
+  .trim()
+  .split("\n");
+
+const input = fileContent.map((line) => line.split(" ").map(Number));
+
+const dp = Array(30)
+  .fill(1)
+  .map(() => Array(30).fill(1));
+
+for (let row = 1; row < 30; row++) {
+  for (let col = row + 1; col < 30; col++) {
+    dp[row][col] = row === 1 ? col : dp[row][col - 1] + dp[row - 1][col - 1];
+  }
+}
+
+input.forEach((i) => console.log(dp[i[0]][i[1]]));
