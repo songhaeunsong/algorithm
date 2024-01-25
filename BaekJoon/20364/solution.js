475e8e6319677641fd779c1977cb3fdcba4fa882
songhaeunsong <gkdms3469@naver.com>
2024-01-25 17:03:25 +0900

    sol: 20364 부동산 다툼

--- BaekJoon/20364/solution.js
+++ BaekJoon/20364/solution.js
@@ -1,31 +1,26 @@
 const fs = require("fs");
-const input = fs
+const [N, ...input] = fs
   .readFileSync("./input.txt")
   .toString()
   .trim()
-  .split("\n")
-  .slice(1)
-  .map(Number);
+  .split("\n");
 
 const visited = {};
 
 let result = "";
 
 input.forEach((des) => {
-  let next = des;
-  let occupied = [0];
-  while (next !== 0) {
-    next = Math.floor(next / 2);
-
+  let next = Number(des);
+  let occupied = 0;
+  while (next !== 1) {
     if (visited[next]) {
-      occupied.push(next);
+      occupied = next;
     }
-    if (next === 1) {
-      result += occupied[occupied.length - 1] + "\n";
-      visited[des] = true;
-      break;
-    }
+    next = Math.floor(next / 2);
   }
+
+  result += occupied + "\n";
+  visited[Number(des)] = true;
 });
 
 console.log(result);
