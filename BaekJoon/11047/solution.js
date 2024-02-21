0149179755a226fa8dd12615931bc22fd6dd9e94
songhaeunsong <gkdms3469@naver.com>
2024-02-22 01:29:40 +0900

    sol: 동전 0

---
+++ BaekJoon/11047/solution.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const [n, ...input] = fs.readFileSync("./input.txt").toString().split("\n");
+let [N, K] = n.split(" ").map(Number);
+
+let answer = 0;
+for (let i = input.length - 1; i >= 0; i--) {
+  const coin = Number(input[i]);
+  const count = Math.floor(K / coin);
+  answer += count;
+  K = Math.floor(K % coin);
+}
+
+console.log(answer);
