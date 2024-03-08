833e2e7ef8edbc03f0ef74279404e49d50be84e4
songhaeunsong <gkdms3469@naver.com>
2024-03-08 15:20:52 +0900

    Sol: 뒤집기

--- BaekJoon/1439/solution.js
+++ BaekJoon/1439/solution.js
@@ -7,4 +7,6 @@ let count = 0;
 for (let i = 0; i < input.length - 1; i++) {
   if (input[i] !== input[i + 1]) count++;
 }
-console.log(count % 2 === 0 ? count / 2 : (count + 1) / 2);
+if (count % 2 === 1) count++;
+
+console.log(count / 2);
