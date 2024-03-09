979ed3759ea555f5fa6440a1607c70782c34c4d6
songhaeunsong <gkdms3469@naver.com>
2024-03-09 23:51:53 +0900

    Sol: 가장 큰 수

---
+++ Programmers/LV2/solution40.js
@@ -0,0 +1,10 @@
+// 가장 큰 수
+
+function solution(numbers) {
+  let answer = numbers
+    .map((num) => num.toString())
+    .sort((a, b) => b + a - (a + b))
+    .join("");
+
+  return answer[0] === "0" ? "0" : answer;
+}
