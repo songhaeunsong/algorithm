23bedb273fbfca964bec66ef7416bef390292907
songhaeunsong <gkdms3469@naver.com>
2024-12-09 15:19:36 +0900

    힙(최소힙) 구현

--- BaekJoon/heap.js
+++ BaekJoon/heap.js
@@ -1,73 +1,55 @@
-// 힙 클래스 초기화
-
 class MinHeap {
   constructor() {
-    // 힙을 저장할 배열
     this.heap = [];
   }
 
-  // 힙의 크기를 반환하는 메서드
   size() {
     return this.heap.length;
   }
-
-  // 두 값을 바꿔주는 메서드
-  swap(idx1, idx2) {
-    [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]];
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
   }
-
-  // 삽입 연산 (bubbleUp)
-
-  add(value) {
-    this.heap.push(value);
-    this.bubbleUp();
-  }
-
-  bubbleUp() {
-    let index = this.heap.length - 1;
-    let parentIdx = Math.floor((index - 1) / 2);
-    while (
-      this.heap[parentIdx] &&
-      this.heap[index][1] < this.heap[parentIdx][1]
-    ) {
-      this.swap(index, parentIdx);
-      index = parentIdx;
-      parent = Math.floor((index - 1) / 2);
-    }
-  }
-
-  // 삭제 연산 (bubbleDown)
-
   poll() {
-    if (this.heap.length === 1) {
-      return this.heap.pop();
-    }
+    if (this.heap.length <= 1) return this.heap.pop();
     const value = this.heap[0];
     this.heap[0] = this.heap.pop();
     this.bubbleDown();
+
     return value;
   }
-  bubbleDown() {
-    let index = 0;
-    let leftIdx = index * 2 + 1;
-    let rightIdx = index * 2 + 2;
-    while (
-      (this.heap[leftIdx] && this.heap[leftIdx][1] < this.heap[index][1]) ||
-      (this.heap[rightIdx] && this.heap[rightIdx][1] < this.heap[index][1])
-    ) {
-      let smallerIdx = leftIdx;
-      if (
-        this.heap[rightIdx] &&
-        this.heap[rightIdx][1] < this.heap[smallerIdx][1]
-      )
-        smallerIdx = rightIdx;
-    }
+  add(a) {
+    this.heap.push(a);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.heap.length - 1;
+    let parent = parseInt((idx - 1) / 2);
 
-    this.swap(index, smallerIdx);
-    index = smallerIdx;
-    leftIdx = index * 2 + 1;
-    rightIdx = index * 2 + 2;
+    while (idx > 0) {
+      if (this.heap[idx] >= this.heap[parent]) break;
+
+      this.swap(idx, parent);
+      idx = parent;
+      parent = parseInt((idx - 1) / 2);
+    }
+  }
+  bubbleDown() {
+    let idx = 0;
+    let left = (idx + 1) * 2 - 1;
+    let right = (idx + 1) * 2;
+
+    while (1) {
+      let smaller = idx;
+      if (this.heap[left] && this.heap[left] < this.heap[idx]) smaller = left;
+      if (this.heap[right] && this.heap[right] < this.heap[idx])
+        smaller = right;
+
+      if (smaller === idx) break;
+
+      this.swap(smaller, idx);
+      idx = smaller;
+      left = (idx + 1) * 2 - 1;
+      right = (idx + 1) * 2;
+    }
   }
 }
-
-// 참고: https://chamdom.blog/heap-using-js/
